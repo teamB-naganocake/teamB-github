f0e7bf2830f93b3eacf7ea58886b4de2892dcf09
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2024-01-21 05:09:10 +0000

    [Update] Admin:Order List

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,8 +7,26 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
-    
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+
+    if @order.update(order_params)
+      if @order.payment_confirmation?
+        @order_details.update_all(making_status: 1)
+      elsif @order_details.in_process?
+        @order.update(status: 2)
+      elsif 
+        
+      end
+      redirect_to admin_order_path(order)
+    else
+      render admin_order_path(order)
+    end
   end
 
+  private
 
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
